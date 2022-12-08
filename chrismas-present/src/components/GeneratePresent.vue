df710e7ed624362e9532836cd48a945dd2594a26
mathieu <mathieu.cans@orange.com>
2022-12-08 22:44:24 +0100

    Generates present links

---
+++ chrismas-present/src/components/GeneratePresent.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+</script>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      presents: null
+    }
+  },
+  methods: {
+    generate() {
+      this.presents = [
+          {from :'marc', to:'mathieu'},
+          {from :'marc', to:'mathieu'},
+      ]
+    },
+    linkFor(){
+      return 'present/mat/ju'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">
+      GENERATEUR DE CADEAUX
+    </h1>
+    <button @click="generate">Générer</button>
+    <div v-for="present in presents">
+      <router-link :to="'/present/' + present.from + '/' + present.to">Le cadeau qu'offrira {{present.from}} </router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
--- chrismas-present/src/main.ts
+++ chrismas-present/src/main.ts
@@ -4,13 +4,13 @@ import App from './App.vue'
 import './assets/main.css'
 import {createRouter, createWebHistory} from "vue-router"
 
-import TheWelcome from "@/components/TheWelcome.vue";
+import GeneratePresent from "@/components/GeneratePresent.vue";
 import PresentFor from "@/components/PresentFor.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: TheWelcome },
+        { path: '/', component: GeneratePresent },
         { path: '/present/:from/:to', component: PresentFor },
     ],
 
