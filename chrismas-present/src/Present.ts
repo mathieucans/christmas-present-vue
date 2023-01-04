48852c254b85eb8214da65269c0a6ca68178e20f
mathieu <mathieu.cans@orange.com>
2023-01-04 19:19:43 +0100

    Add jest configuration to be able to test type script Fix algorithm by asking chat GPT 🫣

--- chrismas-present/src/Present.ts
+++ chrismas-present/src/Present.ts
@@ -6,14 +6,32 @@ export class Present {
     }
 }
 
-export function presentGenerator(participants:string[]) {
+/**
+ *
+ * generated with ChatGPT by the following question:
+ * "an algo that determine who will give a gift to another in an assembly"
+ * <code>
+ * # participants is a list of strings representing the names of the participants
+ *
+ * # Shuffle the list of participants randomly
+ * shuffle(participants)
+ *
+ * # Iterate through the participants
+ * for i, participant in enumerate(participants):
+ *   # Assign the next participant in the list as the recipient of the gift
+ *   recipient = participants[(i + 1) % len(participants)]
+ *   assign_gift_recipient(participant, recipient)
+ *  </code>
+ * @param participants
+ */
+export function presentGenerator(participants: string[]) {
     const result = new Array<Present>();
-    for (const participant of participants) {
-        const alreadyDone = result.map(s => s.to);
-        const presentFor = participants.filter(to => to !== participant)
-            .filter(to => !alreadyDone.includes(btoa(to)));
-        const toIndex = Math.floor(Math.random() * presentFor.length)
-        result.push(new Present(participant, btoa(presentFor[toIndex])))
+    const shuffle = participants.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < shuffle.length; i++) {
+        const participant = shuffle[i];
+        const recipient = shuffle[(i + 1) % shuffle.length];
+
+        result.push(new Present(participant, btoa(recipient)));
     }
     return result
 }
--- chrismas-present/test/Present.spec.ts
+++ chrismas-present/test/Present.spec.ts
@@ -1,40 +1,37 @@
-// class Present{
-//     constructor(
-//         public readonly from : string,
-//         public readonly to : string,
-//     ) {
-//     }
-// }
-// function presentGenerator(participants:string[]) {
-//     const result = new Array<Present>();
-//     for (const participant of participants) {
-//         let alreadyDone = result.map(s => s.to);
-//         const presentFor = participants.filter(to => to !== participant)
-//             .filter(to => !alreadyDone.includes(to));
-//         const toIndex = Math.floor(Math.random() * presentFor.length)
-//         result.push(new Present(participant, btoa(presentFor[toIndex])))
-//
-//     }
-//     return result
-// }
-//
-// describe('PresentGenerator', () => {
-//     const participants = [
-//         "Mich",
-//         "Mu",
-//         "Thomas",
-//         "Claudine",
-//         "Denis",
-//         "Julie",
-//         "Céline",
-//         "Mathieu",
-//     ];
-//     test("all partaicipant should received a present", () => {
-//
-//         expect(presentGenerator(participants).map(p => atob(p.to)).sort()).toEqual(participants.sort())
-//     });
-//
-//     test('nobody offers a present to himself', () => {
-//         expect(presentGenerator(participants).filter(p => p.from == p.to)).toEqual([])
-//     });
-// });
+import {describe, expect, test} from '@jest/globals';
+import {presentGenerator} from "../src/Present";
+
+
+function repeat(n: number, action: () => void) {
+    for (let i = 0; i < n; i++) {
+        action();
+    }
+}
+
+describe('PresentGenerator', () => {
+    const participants = [
+        "Mich",
+        "Mu",
+        "Thomas",
+        "Claudine",
+        "Denis",
+        "Julie",
+        "Céline",
+        "Mathieu",
+    ];
+    test("all participant should received a present", () => {
+        repeat(100, () => {
+            const presents = presentGenerator(participants);
+            expect(presents.map(p => atob(p.to)).sort())
+                .toEqual(participants.sort())
+        })
+    });
+
+    test('nobody offers a present to himself', () => {
+        repeat(100, () => {
+            expect(presentGenerator(participants)
+                .filter(p => p.from == p.to))
+                .toEqual([])
+        })
+    });
+});
